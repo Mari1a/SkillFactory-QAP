60966bbf3f4a07c472b8fe03f43087e8ca9a4e2c
Mari1a <123752321+Mari1a@users.noreply.github.com>
2023-04-04 00:36:45 +0500

    Create object.py

---
+++ object.py
@@ -0,0 +1,7 @@
+from cat import Cat
+
+cat_1 = Cat("Барон", "мальчик", 2)
+cat_2 = Cat("Сэм", "мальчик", 2)
+
+print(f"кот №1 по имени {cat_1.name} пол:{cat_1.gender} возраст:{cat_1.age} года")
+print(f"кот №2 по имени {cat_2.name} пол:{cat_2.gender} возраст:{cat_2.age} года")
